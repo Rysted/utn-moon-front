6c03f796f5cfcbfd187b59b608f19634111f7c23
SeezDev <sebastianszz@hotmail.com>
2023-09-15 17:59:04 -0300

    antes de probar el loadin

---
+++ src/components/Search/FormSearch.jsx
@@ -0,0 +1,90 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../context/ProductsContext";
+import { Link /* Form */ } from "react-router-dom";
+
+import { Explore } from "../Explore/Explore";
+import searchIcon from "../../assets/images/icons/search.svg";
+
+export const FormSearch = ({}) => {
+  const { games, isLoading, error } = useContext(ProductsContext);
+
+  //! Variables de estado para el buscador
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  //! Función para abrir la lista de productos
+  const getGamesFilter = (query) => {
+    setSearchQuery(query);
+    // Filtrar los productos basados en la query
+    const gamesFilter = games.filter((game) =>
+      game.title.toLowerCase().includes(query)
+    );
+    setFilteredProducts(gamesFilter);
+  };
+  const onCloseList = () => {
+    setSearchQuery("");
+    setFilteredProducts([]);
+  };
+
+  const displayedProducts = filteredProducts.slice(0, 3);
+
+  const sliceTitle = (title) => {
+    return title.length > 30 ? `${title.slice(0, 30)}...` : title;
+  };
+
+  console.log("render");
+  return (
+    <form method="post" action="/DEFINIR" className="header__form">
+      <div className="header__form-container">
+        <div className="header__form-label">
+          <input
+            value={searchQuery}
+            onChange={(e) => getGamesFilter(e.target.value.toLowerCase())}
+            type="search"
+            placeholder="buscar en la tienda"
+            name="search"
+            id="search"
+            className="header__form-input"
+          />
+          {!searchQuery && (
+            <img
+              src={searchIcon}
+              alt="Icono de búsqueda"
+              className="header__form-icon"
+            />
+          )}
+          {searchQuery && (
+            <nav className="product-list">
+              <ul className="product-list__items">
+                {displayedProducts.map(({ id, img, title }) => (
+                  <li key={id} className="product-list__item">
+                    <Link
+                      to={`detail/${id}`}
+                      className="product-list__link"
+                      onClick={onCloseList}
+                    >
+                      <img
+                        src={img}
+                        alt={title}
+                        className="product-list__image"
+                      />
+                      <span className="product-list__name">
+                        {sliceTitle(title)}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+                <li className="product-list__results">
+                  <span className="product-list__name">
+                    Resultados: {filteredProducts.length}
+                  </span>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+      </div>
+      <Explore />
+    </form>
+  );
+};
