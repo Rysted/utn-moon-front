1d5b5fccaa4520dec3c07be59d6430403bbdb571
SeezDev <sebastianszz@hotmail.com>
2023-09-20 00:36:43 -0300

    eliminando borrador

--- borrador(borrar)/initialProduct.jsx
+++
@@ -1,9 +0,0 @@
-export const initialProduct = {
-  id: 0,
-  name: "",
-  air_date: "",
-  episode: "",
-  characters: [],
-  url: "",
-  created: "",
-};
--- borrador(borrar)/productsService.jsx
+++
@@ -1,6 +0,0 @@
-export const getAllEpisodes = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/episode");
-
-  const data = await response.json();
-  return data;
-};
